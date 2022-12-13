4155a5efdff0ff192370ca34d7485adb30bf7bce
Connor Larkin <connor.larkin1@gmail.com>
2022-12-13 09:21:24 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -65,10 +65,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          bbb
         </a>
       </footer>
     </div>
