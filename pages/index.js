2d0a7662fe6a1f8e792386bad457ec82441adcb2
Connor Larkin <c@connorlarkin.dev>
2022-12-15 19:13:16 -0600

    firebase

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useTheme as useNextTheme } from 'next-themes'
@@ -7,9 +8,37 @@ import Script from 'next/script'
 import styles from '../styles/Home.module.css'
 
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBf3BhZKvqIaUwOHRnqgm89qUbuoklcLrM",
+  authDomain: "bitbybite-dotxyz.firebaseapp.com",
+  projectId: "bitbybite-dotxyz",
+  storageBucket: "bitbybite-dotxyz.appspot.com",
+  messagingSenderId: "338156763075",
+  appId: "1:338156763075:web:244b7e02af1c899b5cef76",
+  measurementId: "G-1E858JGC2R"
+};
+
+// Initialize Firebase
+
+
 export default function Home() {
   const { setTheme } = useNextTheme();
   const { isDark, type } = useTheme();
+
+
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  },[])
+
 
   return (
     <div className={styles.container}>
