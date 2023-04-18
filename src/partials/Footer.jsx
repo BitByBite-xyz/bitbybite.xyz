8a51caa6fce0a15a47ed9f3cc29f2b47b56c15b5
Jacob Sussman <116610914+Jacob-Sussman@users.noreply.github.com>
2023-04-18 14:13:45 -0500

    Part of footer is not hidden
    - part of footer that is unneeded is now hidden

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -6,11 +6,9 @@ function Footer() {
     <footer>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         {/* Top area: Blocks */}
-        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
-          {/* 1st block */}
+        {/* <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
           <div className="sm:col-span-12 lg:col-span-3">
             <div className="mb-2">
-              {/* Logo */}
               <Link to="/" className="inline-block" aria-label="Cruip"></Link>
             </div>
             <div className="text-sm text-gray-600">
@@ -30,7 +28,6 @@ function Footer() {
             </div>
           </div>
 
-          {/* 2nd block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
             <h6 className="text-gray-800 font-medium mb-2">Products</h6>
             <ul className="text-sm">
@@ -61,7 +58,6 @@ function Footer() {
             </ul>
           </div>
 
-          {/* 3rd block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
             <h6 className="text-gray-800 font-medium mb-2">Resources</h6>
             <ul className="text-sm">
@@ -92,7 +88,6 @@ function Footer() {
             </ul>
           </div>
 
-          {/* 4th block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
             <h6 className="text-gray-800 font-medium mb-2">Company</h6>
             <ul className="text-sm">
@@ -123,7 +118,6 @@ function Footer() {
             </ul>
           </div>
 
-          {/* 5th block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
             <h6 className="text-gray-800 font-medium mb-2">Subscribe</h6>
             <p className="text-sm text-gray-600 mb-4">
@@ -164,13 +158,11 @@ function Footer() {
                       </svg>
                     </button>
                   </div>
-                  {/* Success message */}
-                  {/* <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p> */}
                 </div>
               </div>
             </form>
           </div>
-        </div>
+        </div> */}
 
         {/* Bottom area */}
         <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
